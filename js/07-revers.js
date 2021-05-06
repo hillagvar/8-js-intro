b14751863c69af5f1c5b19fc644098806e9c6e21
Hila <hilla.gvar@gmail.com>
2021-05-06 18:25:13 +0300

    reverse

---
+++ js/07-revers.js
@@ -0,0 +1,34 @@
+/*
+Duotas array su text reiksmemis (string)
+Reikia: sujungti juos atbulai iterpiant ", "
+*/
+
+// labas, rytas, tau, sakau
+const d = ['sakau', 'tau', 'rytas', 'labas'];
+let reverse = '';
+
+reverse += d[3];
+reverse += ', ';
+reverse += d[2];
+reverse += ', ';
+reverse += d[1];
+reverse += ', ';
+reverse += d[0];
+
+console.log(reverse);
+console.log('-----------');
+
+reverse = d[3] + ', ' + d[2] + ', ' + d[1] + ', ' + d[0];
+
+console.log(reverse);
+console.log('-----------');
+
+reverse = `${d[3]}, ${d[2]}, ${d[1]}, ${d[0]}`;
+
+console.log(reverse);
+console.log('-----------');
+
+const d2 = [...d];
+const jras = d2.reverse().join(', ');
+
+console.log(jras);
